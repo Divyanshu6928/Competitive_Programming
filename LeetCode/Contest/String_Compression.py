f3819a3f242917fc790c484cc2b45ff974307674
Divyanshu <lyricalv15@gmail.com>
2024-05-26 08:25:16 +0530

    CP

---
+++ LeetCode/Contest/String_Compression.py
@@ -0,0 +1,20 @@
+word=input()
+a=""
+comp = ""
+i = 0
+n = len(word)
+    
+while i < n:
+    c = word[i]
+    count = 0
+    
+    while i < n and word[i] == c and count < 9:
+        i += 1
+        count += 1
+        
+    comp += str(count) + c
+    
+print(comp)
+
+
+
